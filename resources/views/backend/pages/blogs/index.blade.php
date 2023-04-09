af78703ad23a2d326f66033f69e77077178c460b
GulgazGasimova <gulia.gasimova@mail.ru>
2023-04-09 20:23:49 +0400

    blog

--- resources/views/backend/pages/blogs/index.blade.php
+++ resources/views/backend/pages/blogs/index.blade.php
@@ -54,12 +54,8 @@
                                             <td>{{$blog->title}}</td>
                                             <td>{{$blog->description}}</td>
                                             <td>{{$blog->content}}</td>
-<<<<<<< .merge_file_si8Hpp
                                             <td><img src="{{ asset(  "$blog->image") }}" width="50" height="50"></td>
-=======
                                             <td><img src="{{ asset('blogs_images/' . $blog->image) }}" width="50" height="50"></td>
->>>>>>> .merge_file_c1zXdq
-
                                             <td>{{$blog->url}}</td>
                                             <td>{{$blog->cat_id }}</td>
                                             <td>{{$blog->status}}</td>
